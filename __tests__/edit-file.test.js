e3391b4ca5bc9cddcc39f0520dcbdf285bca511b
holliemaethomas <holliemaethomas85@gmail.com>
2018-11-19 19:09:29 -0800

    fix path

--- __tests__/edit-file.test.js
+++ __tests__/edit-file.test.js
@@ -1,7 +1,7 @@
 'use strict';
 jest.mock('fs');
 
-const reader = require('../lib/reader-callbacks.js');
+const reader = require('../lib/reader-callbacks-array.js');
 
 describe('File Reader Module', () => {
   it('when given a bad file, returns an error', done => {
